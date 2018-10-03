cf919a6ebae6bcf64ac817eca93db24d4193a52b
vvgediya <vvgediya@bitbucket.org>
2018-10-03 12:49:34 +0530

    [FIX] year issue in birthday wish module

--- fl_birthday_wish/models/hr_employee.py
+++ fl_birthday_wish/models/hr_employee.py
@@ -13,12 +13,12 @@ class HrEmployee(models.Model):
         for employee in self.env['hr.employee'].search([]):
             if employee.birthday:
                 emp_birthdate = datetime.strptime(employee.birthday, '%Y-%m-%d').date()
-                if str(today_date) == str(emp_birthdate):
+                if today_date.day == emp_birthdate.day and today_date.month == emp_birthdate.month:
                     template_id = self.env.ref('fl_birthday_wish.email_birthday_wishes_employee_template')
                     template_id.send_mail(employee.id, force_send=True)
         for partner in self.env['res.partner'].search([]):
             if partner.birthday:
                 cust_birthdate = datetime.strptime(partner.birthday, '%Y-%m-%d').date()
-                if str(today_date) == str(cust_birthdate):
+                if today_date.day == cust_birthdate.day and today_date.month == cust_birthdate.month:
                     template_id = self.env.ref('fl_birthday_wish.email_birthday_wishes_partner_template')
                     template_id.send_mail(partner.id, force_send=True)
