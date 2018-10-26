dcdf7a388516326c59a2951619a92f8e3ee4a976
thefuturelens <thefuturelens.com>
2018-10-26 11:58:37 +0530

    [IMP] add access right for product

--- fl_quick_task/models/project_task.py
+++ fl_quick_task/models/project_task.py
@@ -8,7 +8,7 @@ from odoo import api, fields, models, _
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    date_deadline = fields.Date(string='Deadline', index=True, default=datetime.now())
+    date_deadline = fields.Date(string='Deadline', default=lambda self: fields.Date.today())
 
     @api.multi
     def create_task_dialog(self):
